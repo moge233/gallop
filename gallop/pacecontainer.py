c9694ef1fe5e2ba2e50dbd3a32de84ed4292f164
mathe <mathewlogan90@gmail.com>
2025-10-18 19:09:34 -0400

    Commitin' for fun

--- gallop/pacecontainer.py
+++ gallop/pacecontainer.py
@@ -1,36 +1,53 @@
 #! python3
 
 
+from datetime import date
 from math import ceil, floor
 
 from brispy.singlefile import SingleFileHorse, SingleFilePastPerformance
-from horsedb2 import get_average_variant
+from chart_parser.special_types import Course
+from horsedb2.variants import get_average_variant
 
 
 DEFAULT_MIN_ROUTE_DISTANCE: float = 8.0
 
 
 class PaceContainerPastPerformance:
-    def __init__(self, name: str, sfpp: SingleFilePastPerformance):
-        self.name = name
+    def __init__(self, sfpp: SingleFilePastPerformance):
         self.distance = round(abs(sfpp.distance) / 220.0, 2)
-        self.surface = sfpp.surface
-        self.all_weather = True if sfpp.all_weather_surface_flag == 'A' else False
-        self.track_condition = sfpp.track_condition
-        self.track_code = sfpp.track_code
-        self.finish_position = sfpp.finish_position
+        if (sfpp.surface == 'D' or (sfpp.surface == 'T' and 'x' in sfpp.start_code)) and \
+                sfpp.all_weather_surface_flag != 'A':
+            self.course: Course | None = Course.DIRT
+        elif sfpp.surface == 'D' and sfpp.all_weather_surface_flag == 'A':
+            self.course: Course | None = Course.ALL_WEATHER_TRACK
+        elif sfpp.surface == 'T':
+            self.course: Course | None = Course.TURF
+        elif sfpp.surface == 'd':
+            self.course: Course | None = Course.INNER_TURF
+        elif sfpp.surface == 't':
+            self.course: Course | None = Course.OUTER_TURF
+        else:
+            print(sfpp.surface)
+            self.course: Course | None = None
         self.t1 = sfpp.two_furlong_fraction if self.distance < DEFAULT_MIN_ROUTE_DISTANCE \
             else sfpp.four_furlong_fraction
         self.t2 = sfpp.four_furlong_fraction if self.distance < DEFAULT_MIN_ROUTE_DISTANCE \
             else sfpp.six_furlong_fraction
         self.t3 = sfpp.final_time
         self.track_variant = sfpp.track_variant
-        self.average_variant = get_average_variant(self.track_code, self.distance, self.surface,
-                                                   sfpp.all_weather_surface_flag)
+        if sfpp.track_code:
+            try:
+                self.average_variant = int(get_average_variant(sfpp.track_code, sfpp.distance, sfpp.surface,
+                                                               sfpp.all_weather_surface_flag))
+            except Exception as e:
+                print(e)
+                self.average_variant = 17
+        else:
+            self.average_variant = 0
         self.bl1 = sfpp.first_call_beaten_lengths
         self.bl2 = sfpp.second_call_beaten_lengths
         self.bl3 = sfpp.finish_beaten_lengths
-        self.winner = 1 if self.finish_position == '1' else 0
+        self.winner = 1 if sfpp.finish_position == '1' else 0
 
         if abs(self.average_variant - self.track_variant) > 1:  # type: ignore
             if self.average_variant - self.track_variant > 4:
@@ -62,54 +79,54 @@ class PaceContainerPastPerformance:
 
         if self.distance < DEFAULT_MIN_ROUTE_DISTANCE:
             if not self.t1:
-                self.fr1 = '-'
-                self.fr2 = '-'
-                self.fr3 = '-'
-                self.ep = '-'
-                self.sp = '-'
-                self.ap = '-'
-                self.fx = '-'
-                self.energy = '-'
+                self.f1 = 0
+                self.f2 = 0
+                self.f3 = 0
+                self.ep = 0
+                self.sp = 0
+                self.ap = 0
+                self.fx = 0
+                self.energy = 0
             else:
-                self.fr1 = round((1320 - 10 * self.bl1) / adj_t1, 2)
-                self.fr2 = round((1320 - 10 * (self.bl2 - self.bl1)) / (adj_t2 - adj_t1), 2)
-                self.fr3 = round((660 * (self.distance - 4) - 10 * (self.bl3 - self.bl2)) / (adj_t3 - adj_t2), 2)
+                self.f1 = round((1320 - 10 * self.bl1) / adj_t1, 2)
+                self.f2 = round((1320 - 10 * (self.bl2 - self.bl1)) / (adj_t2 - adj_t1), 2)
+                self.f3 = round((660 * (self.distance - 4) - 10 * (self.bl3 - self.bl2)) / (adj_t3 - adj_t2), 2)
                 self.ep = round((2640 - 10 * self.bl2) / adj_t2, 2)
-                self.sp = round((self.ep + self.fr3) / 2, 2)
-                self.ap = round((self.fr1 + self.fr2 + self.fr3) / 3, 2)
-                self.fx = round((self.fr1 + self.fr3) / 2, 2)
-                self.energy = round(self.ep / (self.ep + self.fr3), 4)
+                self.sp = round((self.ep + self.f3) / 2, 2)
+                self.ap = round((self.f1 + self.f2 + self.f3) / 3, 2)
+                self.fx = round((self.f1 + self.f3) / 2, 2)
+                self.energy = round(self.ep / (self.ep + self.f3), 4)
         else:
             if not self.t1:
-                self.fr1 = '-'
-                self.fr2 = '-'
-                self.fr3 = '-'
-                self.ep = '-'
-                self.sp = '-'
-                self.ap = '-'
-                self.fx = '-'
-                self.energy = '-'
+                self.f1 = 0
+                self.f2 = 0
+                self.f3 = 0
+                self.ep = 0
+                self.sp = 0
+                self.ap = 0
+                self.fx = 0
+                self.energy = 0
             else:
-                self.fr1 = round((2640 - 10 * self.bl1) / adj_t1, 2)
-                self.fr2 = round((1320 - 10 * (self.bl2 - self.bl1)) / (adj_t2 - adj_t1), 2)
-                self.fr3 = round((660 * (self.distance - 6) - 10 * (self.bl3 - self.bl2)) / (adj_t3 - adj_t2), 2)
+                self.f1 = round((2640 - 10 * self.bl1) / adj_t1, 2)
+                self.f2 = round((1320 - 10 * (self.bl2 - self.bl1)) / (adj_t2 - adj_t1), 2)
+                self.f3 = round((660 * (self.distance - 6) - 10 * (self.bl3 - self.bl2)) / (adj_t3 - adj_t2), 2)
                 self.ep = round((3960 - 10 * self.bl2) / adj_t2, 2)
-                self.sp = round((self.ep + self.fr3) / 2, 2)
-                self.ap = round((self.fr1 + self.fr3) / 2, 2)
-                self.fx = round((self.fr1 + self.fr3) / 2, 2)
-                self.energy = round(self.ep / (self.ep + self.fr3), 4)
+                self.sp = round((self.ep + self.f3) / 2, 2)
+                self.ap = round((self.f1 + self.f3) / 2, 2)
+                self.fx = round((self.f1 + self.f3) / 2, 2)
+                self.energy = round(self.ep / (self.ep + self.f3), 4)
 
     def __str__(self):
         ret = ''
         for k, v in vars(self).items():
             ret += f'{k}={v}, '
-        return f'BrohamerPastPerformance({ret[:-2]})'
+        return f'PaceContainerPastPerformance({ret[:-2]})'
 
     def __repr__(self):
         ret = ''
         for k, v in vars(self).items():
             ret += f'{k}={v}, '
-        return f'BrohamerPastPerformance({ret[:-2]})'
+        return f'PaceContainerPastPerformance({ret[:-2]})'
 
     def is_winner(self) -> bool:
         return self.winner == 1
@@ -117,19 +134,16 @@ class PaceContainerPastPerformance:
 
 class PaceContainer:
     def __init__(self, sfh: SingleFileHorse):
-        self.name = sfh.name
-        self.post_position = sfh.post_position
         self.past_performances: list[PaceContainerPastPerformance] = []
         if sfh.past_performances:
             for pp in sfh.past_performances:
-                if not pp.track_code or int(pp.date[:4]) < 2025 or pp.two_furlong_fraction == 0 \
-                        or pp.four_furlong_fraction == 0:
+                if not pp.track_code or int(pp.date[:4]) < date.today().year:
                     continue
-                self.past_performances.append(PaceContainerPastPerformance(self.name, pp))
+                self.past_performances.append(PaceContainerPastPerformance(pp))
         if self.past_performances:
-            fr1s = [float(pp.fr1) for pp in self.past_performances]
-            fr2s = [float(pp.fr2) for pp in self.past_performances]
-            fr3s = [float(pp.fr3) for pp in self.past_performances]
+            fr1s = [float(pp.f1) for pp in self.past_performances]
+            fr2s = [float(pp.f2) for pp in self.past_performances]
+            fr3s = [float(pp.f3) for pp in self.past_performances]
             eps = [float(pp.ep) for pp in self.past_performances]
             sps = [float(pp.sp) for pp in self.past_performances]
             aps = [float(pp.ap) for pp in self.past_performances]
--- gallop/utility.py
+++ gallop/utility.py
@@ -7,10 +7,16 @@ import os
 from pandas import DataFrame, concat
 from scipy.stats import norm
 
+from brispy.singlefile import SingleFile, SingleFileHorse, SingleFilePastPerformance, SingleFileRace, SingleFileRow
 from chart_parser.chart import Chart
 from chart_parser.horse import Horse
 from chart_parser.race import Race
+from chart_parser.special_types import Course
 from chart_parser.utils import parse_chart
+from gallop.pacecontainer import PaceContainerPastPerformance
+
+
+DEFAULT_PP_DATA_DIR: str = 'C:\\Users\\mathe\\OneDrive\\Documents\\horses\\pp_data\\2025'
 
 
 #######################################################################
@@ -49,7 +55,7 @@ def summarize_post_positions(charts: list[Chart]) -> None:
             round(expected_wins, 2),
             actual_wins,
             round(variance, 2),
-            round(norm.cdf(actual_wins, loc=expected_wins, scale=variance), 2)
+            round(float(norm.cdf(actual_wins, loc=expected_wins, scale=variance)), 2)
         )
 
 
@@ -94,3 +100,169 @@ def chart_to_dataframe(chart: Chart, skip_races: bool) -> DataFrame:
     else:
         filtered_dict = dict(vars(chart).items())
     return DataFrame([filtered_dict])
+
+
+def check_if_pp_exists(track_code: str, race_date: date) -> bool:
+    if len(track_code) == 2:
+        track_code += 'X'
+    file_name = f'{track_code}{race_date.strftime('%m%d')}.drf'
+    if os.path.exists(os.path.join(DEFAULT_PP_DATA_DIR, file_name)):
+        return True
+    return False
+
+
+def get_pp_path(track_code: str, race_date: date) -> str:
+    if len(track_code) == 2:
+        track_code += 'X'
+    file_name = f'{track_code}{race_date.strftime('%m%d')}.drf'
+    return os.path.join(DEFAULT_PP_DATA_DIR, file_name)
+
+
+#######################################################################
+# SingleFile(...) utils
+#######################################################################
+def is_maiden(race: SingleFileRace) -> bool:
+    if 'Md' in race.classification:
+        return True
+    return False
+
+
+def remove_todays_scratches(single_file: SingleFile, todays_scratches: list[str] | None = None) -> list[SingleFileRow]:
+    if not todays_scratches:
+        return single_file.rows
+    else:
+        todays_scratches = [scratch.casefold() for scratch in todays_scratches]
+    for row in single_file.rows:
+        if row.horse.name.casefold() in todays_scratches:
+            print(f'Removing a scratch: {row.horse.name}')
+    return [row for row in single_file.rows if row.horse.name.casefold() not in todays_scratches]
+
+
+def singlefile_past_performance_to_dataframe(sfpp: SingleFilePastPerformance) -> DataFrame:
+    return DataFrame([dict(vars(sfpp).items())])
+
+
+def singlefile_past_performance_to_pace_container_past_performance(sfpp: SingleFilePastPerformance) -> DataFrame:
+    return DataFrame([dict(vars(PaceContainerPastPerformance(sfpp)).items())])
+
+
+def create_dataframe_from_singlefile(single_file: SingleFile, skip_maidens=False) -> list[DataFrame | None]:
+    return []
+
+
+def get_combined_dataframe_from_singlefile_horse_past_performances(horse: SingleFileHorse) -> DataFrame | None:
+    pp_dfs: list[DataFrame | None] = []
+    for pp in horse.past_performances:
+        if not pp.date or pp.distance / 220 > 12 or int(pp.date[:4]) < 2025:
+            continue
+        if not horse.program_number:
+            continue
+        pp_df = singlefile_past_performance_to_dataframe(pp)
+        pc_df = singlefile_past_performance_to_pace_container_past_performance(pp)
+        combined_df = concat([pp_df, pc_df], axis=1, ignore_index=False)
+        combined_df.insert(0, 'horseid', int(horse.program_number))
+        combined_df.insert(0, 'name', horse.name)
+        pp_dfs.append(combined_df)
+    if pp_dfs != []:
+        pp_df = concat(pp_dfs, axis=0, ignore_index=True)
+    else:
+        pp_df = None
+    return pp_df
+
+
+def singlefile_to_combined_dataframe(sf: SingleFile) -> DataFrame | None:
+    current_race_number: int = 0
+    todays_dfs: list[DataFrame | None] = []
+    race_dfs: list[DataFrame | None] = []
+    race_df: DataFrame | None = None
+    for row in sf.rows:
+        if is_maiden(row.race):
+            continue
+        if current_race_number != row.race.number:
+            if race_dfs:
+                race_df = concat(race_dfs, axis=0, ignore_index=True)
+                race_df.insert(0, 'todays_race_number', current_race_number)
+                todays_dfs.append(race_df)
+            current_race_number = row.race.number
+            race_dfs: list[DataFrame | None] = []
+        if row.horse:
+            horse_df = get_combined_dataframe_from_singlefile_horse_past_performances(row.horse)
+            if horse_df is None:
+                continue
+            horse_df.insert(2, "horseno", row.horse.program_number)
+            race_dfs.append(horse_df)
+    if race_dfs:
+        # We need to do all this here to get the final race of the list
+        race_df = concat(race_dfs, axis=0, ignore_index=True)
+        race_df.insert(0, 'todays_race_number', current_race_number)
+        todays_dfs.append(race_df)
+    if todays_dfs:
+        # Create the day's dataframe with the PaceContainer entries added on the end
+        day_df = concat(todays_dfs, axis=0, ignore_index=True)
+        return day_df
+    else:
+        return None
+
+
+def get_all_pace_data(charts_path: str, track_code: str) -> DataFrame | None:
+    charts: list[Chart] = get_charts(charts_path, track_code)
+    charts_dfs: list[DataFrame] = []
+    charts_df: DataFrame | None = None
+    # Now that we have the charts, we need to look and see if we have the DRF (SingleFile) data file
+    # for that day
+    for chart in charts:
+        chart_dfs: list[DataFrame] = []
+        if chart.header:
+            if check_if_pp_exists(track_code, chart.header.race_date):
+                # Now that we have a chart that has a corresponding data file, we need to get the SingleFile
+                # instance of the data file and then get the combined dataframe.
+                data_frame = singlefile_to_combined_dataframe(
+                    SingleFile.create(
+                        get_pp_path(track_code, chart.header.race_date)
+                    )
+                )
+                count = 1
+                for race in chart.races:
+                    if 'Md' in race.abbreviated_race_name or data_frame is None:
+                        # Skip maidens
+                        continue
+                    else:
+                        # It's a race we care about, find the ponies involved
+                        winner_name = race.get_winner().name
+                        race_df = data_frame[data_frame['todays_race_number'] == race.number]
+                        if not race_df.empty:
+                            race_df.insert(0, 'todays_winner', race_df['name'] == winner_name.upper())
+                            race_df.insert(0, 'key',
+                                           int(f'{chart.header.race_date.strftime('%Y%m%d')}{race.number:02}'))
+                            count += 1
+                            race_df = race_df.copy(True)
+                            race_df['todays_winner'] = race_df['todays_winner'].astype(int)
+                            race_df['todays_surface'] = Course.DIRT.name
+                            race_df['todays_distance'] = abs(race.distance)
+                            # TODO: Filter out different surfaces and/or distances
+                            race_df = race_df[race_df['todays_distance'] < 8]  # Sprints only
+                            race_df = race_df.drop_duplicates(subset=['name'], keep='first')
+                            if race_df.shape[0] > 4 and 1 in race_df['todays_winner'].values:
+                                race_df['rank_f1'] = race_df['f1'].rank(method='average', ascending=False)
+                                race_df['rank_f2'] = race_df['f2'].rank(method='average', ascending=False)
+                                race_df['rank_f3'] = race_df['f3'].rank(method='average', ascending=False)
+                                race_df['rank_ep'] = race_df['ep'].rank(method='average', ascending=False)
+                                race_df['rank_sp'] = race_df['sp'].rank(method='average', ascending=False)
+                                race_df['rank_ap'] = race_df['ap'].rank(method='average', ascending=False)
+                                race_df['rank_fx'] = race_df['fx'].rank(method='average', ascending=False)
+                                if 1 in race_df['todays_winner']:
+                                    chart_dfs.append(race_df)
+        if chart_dfs:
+            chart_df = concat(chart_dfs, axis=0, ignore_index=True)
+            charts_dfs.append(chart_df)
+    if charts_dfs:
+        charts_df = concat(charts_dfs, axis=0, ignore_index=True)
+        return charts_df
+    return None
+
+
+def filter_all_pace_data(dataframe: DataFrame) -> DataFrame:
+    ret = dataframe.copy()
+    ret = ret[ret['distance'] < 8]
+    ret = ret[ret['course'] == Course.DIRT]
+    return ret
